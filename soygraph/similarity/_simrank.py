d829c25d4c37db84a0da5392449170cba4ac7ddd
lovit <soy.lovit@gmail.com>
2017-06-26 19:53:26 +0900

    Normalizer as default

--- soygraph/similarity/_simrank.py
+++ soygraph/similarity/_simrank.py
@@ -1,41 +1,40 @@
 class SimRank:
     def __init__(self, graph=None, max_iter=10, decaying_factor=0.85, 
-                 min_similarity=0.005, normalizer=lambda x: max([w for node, w in x]), 
-                 verbose=True):
+                 min_similarity=0.005, verbose=True):
         self.g = graph
         self.max_iter = max_iter
         self.df = decaying_factor
         self.sim = {}
         self.min_similarity = min_similarity
-        self.normalizer = normalizer
         self.verbose = verbose
 
     def train(self, graph=None):
         if graph:
             self.g = graph
         
-        norm = {n:self.normalizer(inbs) for n, inbs in self.g.inb.items()}
+        normalizer=lambda x: sum([w for node, w in x])
+        norm = {n:normalizer(inbs) for n, inbs in self.g.inb.items()}
         
         for n_iter in range(1, self.max_iter+1):
             sim_ = defaultdict(lambda: defaultdict(lambda: 0))
             for a in self.g.nodes():                
                 for b in self.g.nodes():
                     if a == b: continue
-                    inbs_a = self.g.inbounds(a) 
-                    inbs_b = self.g.inbounds(b)                    
+                    inbs_a = self.g.inbounds(a)
+                    inbs_b = self.g.inbounds(b)
                     sim_ab = 0
 
                     for inb_a, w1 in inbs_a:
                         for inb_b, w2 in inbs_b:
-                            sim_ab_ = self.sim.get(inb_a, {}).get(inb_b, 0) if inb_a != inb_b else 1.0          
+                            sim_ab_ = self.sim.get(inb_a, {}).get(inb_b, 0) if inb_a != inb_b else 1.0 
                             sim_ab += sim_ab_ * w1 * w2
-                    sim_ab *= (self.df / (len(inbs_b) * len(inbs_a) * norm[a] * norm[b]))
+                    sim_ab *= (self.df / (norm[a] * norm[b]))
                     if sim_ab >= self.min_similarity:
                         sim_[a][b] = sim_ab
 
             self.sim = {n:dict(sv) for n, sv in sim_.items()}            
             if self.verbose:
-                print('#iter = %d' % n_iter)                
+                print('#iter = %d' % n_iter)
         return self.sim
 
 class SingleVectorSimRank:
