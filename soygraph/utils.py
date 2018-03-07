9ffa909572704bd699968c774ff8190b878bc363
lovit <soy.lovit@gmail.com>
2018-03-07 20:40:25 +0900

    Commit bow_to_graph function

--- soygraph/utils.py
+++ soygraph/utils.py
@@ -1,5 +1,7 @@
 import os
 import psutil
+import numpy as np
+from scipy.sparse import csr_matrix
 
 def get_available_memory():
     """It returns remained memory as percentage"""
@@ -12,3 +14,26 @@ def get_process_memory():
     
     process = psutil.Process(os.getpid())
     return process.memory_info().rss / (1024 ** 3)
+
+def bow_to_graph(x):
+    """It transform doc-term sparse matrix to graph.
+    Vertex = [doc_0, doc_1, ..., doc_{n-1}|term_0, term_1, ..., term_{m-1}]
+    
+    Arguments
+    ---------
+    x: scipy.sparse
+    
+    Returns
+    -------
+    g: scipy.sparse.csr_matrix
+        V` = x.shape[0] + x.shape[1]
+        its shape = (V`, V`) 
+    """
+    x = x.tocsr()
+    x_ = x.transpose().tocsr()
+    data = np.concatenate((x.data, x_.data))
+    indices = x_.indices + x.shape[1]
+    indices = np.concatenate((x.indices, indices))
+    indptr = x_.indptr[1:] + len(x.data)
+    indptr = np.concatenate((x.indptr, indptr))
+    return csr_matrix((data, indices, indptr))
