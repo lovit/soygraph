efd01d2ecdf46e55c6493cd2a24be1775c5f0893
lovit <soy.lovit@gmail.com>
2018-03-08 19:19:01 +0900

    Fix wrong variable name

--- soygraph/similarity/_simrank.py
+++ soygraph/similarity/_simrank.py
@@ -1,43 +1,59 @@
+from collections import defaultdict
+
 class SimRank:
-    def __init__(self, graph=None, max_iter=10, decaying_factor=0.85, 
-                 min_similarity=0.005, max_norm=True, verbose=True):
+    def __init__(self, graph=None, max_iter=10, decaying_factor=0.8,
+        min_similarity=0.005, verbose=True):
+
         self.g = graph
         self.max_iter = max_iter
         self.df = decaying_factor
         self.sim = {}
         self.min_similarity = min_similarity
-        self.max_norm = max_norm
         self.verbose = verbose
 
     def train(self, graph=None):
         if graph:
             self.g = graph
+
+        nodes = self.g.nodes()
         
-        if self.max_norm:
-            norm = {n:max([w for inbn, w in inbs]) for n, inbs in self.g.inb.items()}
-        else:
-            norm = {n:sum([w for inbn, w in inbs]) for n, inbs in self.g.inb.items()}
-        
+        # normalized by inbound weight sum
+        sum_inb_weight = {node:sum([w for _, w in self.g.inbounds(node)])
+            for node in nodes}
+
         for n_iter in range(1, self.max_iter+1):
+
             sim_ = defaultdict(lambda: defaultdict(lambda: 0))
-            for a in self.g.nodes():                
-                for b in self.g.nodes():
-                    if a == b: continue
-                    inbs_a = self.g.inbounds(a) 
-                    inbs_b = self.g.inbounds(b)                    
+
+            for a in nodes:
+                for b in nodes:
+
+                    if a == b:
+                        continue
+
+                    inbs_a = self.g.inbounds(a)
+                    inbs_b = self.g.inbounds(b)
                     sim_ab = 0
 
-                    for inb_a, w1 in inbs_a:
-                        for inb_b, w2 in inbs_b:
-                            sim_ab_ = self.sim.get(inb_a, {}).get(inb_b, 0) if inb_a != inb_b else 1.0          
-                            sim_ab += sim_ab_ * w1 * w2
-                    sim_ab *= (self.df / (len(inbs_b) * len(inbs_a) * norm[a] * norm[b]))
+                    for inb_a, wa in inbs_a:
+                        for inb_b, wb in inbs_b:
+                            if inb_a == inb_b:
+                                sim_ab_ = 1.0
+                            else:
+                                sim_ab_ = self.sim.get(inb_a, {}).get(inb_b, 0)
+
+                            sim_ab += sim_ab_ * wa * wb
+                    sim_ab *= (self.df / (sum_inb_weight[a] * sum_inb_weight[b]))
+
+                    # pruning using minimum similarity to prevent out of memory
                     if sim_ab >= self.min_similarity:
                         sim_[a][b] = sim_ab
 
-            self.sim = {n:dict(sv) for n, sv in sim_.items()}            
+            self.sim = {node:dict(sim_vec) for node, sim_vec in sim_.items()}
+
             if self.verbose:
-                print('#iter = %d' % n_iter)                
+                print('#iter = %d' % n_iter)
+
         return self.sim
 
     
@@ -49,23 +65,23 @@ class SingleVectorSimRank:
         normalizer=lambda x: sum([w for node, w in x])
         self.normi = {n:normalizer(inbs) for n, inbs in self.g.inb.items()}
         self.normo = {n:normalizer(outbs) for n, outbs in self.g.outb.items()}
-        
+
     def query(self, q, topk=10, max_iter=-1):
         if max_iter < 0:
             max_iter = self.max_iter
         tops = {q:1}
         sims = {q:1}
         kth = 0
-        
+
         for n_iter in range(1, max_iter+1):
             df = pow(self.df, n_iter)
-            
+
             sorted_sim = sorted(sims.items(), key=lambda x:x[1], reverse=True)
             kth = sorted_sim[:topk][-1][1]
             delta = (kth - sorted_sim[:topk+1][-1][1])
             if delta > df:
                 break
-            
+
             tops_ = {}
             btrks = {}
             for topb, tbw in tops.items():
@@ -73,27 +89,27 @@ class SingleVectorSimRank:
                     inbw /= self.normi[topb]
                     tops_[inb] = (tops_.get(inb, 0) + tbw * inbw)
                     btrks[topb] = (btrks.get(topb, 0) - tbw * inbw)
-                    
+
             for top, tw in tops_.items():
                 for outb, outbw in self.g.outbounds(top):
                     outbw /= self.normi[outb]
                     if outbw < delta:
                         continue
                     btrks[outb] = (btrks.get(outb, 0) + tw * outbw)
-            
+
             btrks = {n:w for n,w in btrks.items() if w > 0}
             tops = tops_
             if not btrks:
                 continue
-                    
-            for n_back in range(1, n_iter):                
+
+            for n_back in range(1, n_iter):
                 btrks_ = {}
                 for top, tw in btrks.items():
                     for outb, outbw in self.g.outbounds(top):
                         outbw /= self.normi[outb]
                         btrks_[outb] = (btrks_.get(outb, 0) + tw * outbw)
                 btrks = btrks_
-            
+
             for launch, lw in btrks.items():
                 if q != launch:
                     sims[launch] = sims.get(launch, 0) + lw * df
